29e6a1676843394d4bbfd322c4c4a1c7a44005f4
tperita-genius <tpe.rita456@gmail.com>
2026-08-13 15:49:06 +0800

    feat: project rename set

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-frontend' title`, () => {
+  it(`should have the 'ims-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-frontend');
+    expect(app.title).toEqual('ims-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ims-frontend');
   });
 });
